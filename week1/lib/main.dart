96e4a517fd634ab02422973a71dc3ef8e737a1f0
archit bajpai <archit208027@gmail.com>
2023-04-08 16:44:17 +0530

    added another page where you can see the word pairs you liked

--- week1/lib/main.dart
+++ week1/lib/main.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:english_words/english_words.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(create: (context) => MyAppState(),
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
       child: MaterialApp(
         title: 'Namer App',
         theme: ThemeData(
@@ -20,18 +27,34 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
         home: MyHomePage(),
-      ),);
+      ),
+    );
   }
 }
+
+var fav = <WordPair>[];
+
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
-
+  var favourites = <WordPair>[];
   void getNext() {
     current = WordPair.random();
-    print(current);
+    notifyListeners();
+  }
+
+  /* declared an array that stores our favourite word pairs*/
+  void toggleFavorite() {
+    if (favourites.contains(current)) {
+      favourites.remove(current);
+      fav.remove(current);
+    } else {
+      fav.add(current);
+      favourites.add(current);
+    }
     notifyListeners();
   }
 }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -49,25 +72,175 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
 
   @override
-  Widget build(BuildContext context)
-  {
-    var appState= context.watch<MyAppState>();
-    return Scaffold(
-      body: Column(
-        children: [
-          Text('THIS IS MY FIRST FLUTTER APPLICAYTION'),
-          Text('handle this app with care'),
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
 
-          ElevatedButton(onPressed: (){
-           appState.getNext();
-          }, child: Text('Next'),)
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text(
+                      'Home',
+                      textScaleFactor: 2.5,
+                    ),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text(
+                      'Favorites',
+                      textScaleFactor: 2.5,
+                    ),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favourites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text(
+                  'Like',
+                  textScaleFactor: 2.0,
+                ),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text(
+                  'Next',
+                  textScaleFactor: 2.0,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
 }
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!
+        .copyWith(color: Colors.white, fontSize: 100.0);
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first}  ${pair.second}",
+        ),
+      ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favourites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have ' '${appState.favourites.length} favorites:'),
+        ),
+        for (var pair in appState.favourites)
+          ElevatedButton(
+            onPressed: () {
+              var item = pair;
+              fav.remove(item);
+            },
+            child: ListTile(
+              leading: Icon(Icons.delete),
+              title: Text(pair.asLowerCase),
+            ),
+          )
+      ],
+    );
+  }
+}
+
 /*
  Todays date -> 7th April 2023
  This is the completion of a basic flutter project I have added a function getNext which is present inside a class MyAppState and this function is used to generate a random word pair
